7a2d069f5e834e4666e42134235abb71ee67d83b
Rafael Pessoni <rafael.pessoni@outlook.com>
2016-10-25 23:18:07 -0200

    alteração no caminho das aplicações para renomear as pastas

---
+++ SqlServer/src/ToDoList/Migrations/20161026011213_001.Designer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using ToDoList.Model;
+
+namespace ToDoList.Migrations
+{
+    [DbContext(typeof(ToDoListContext))]
+    [Migration("20161026011213_001")]
+    partial class _001
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("ToDoList.Model.Tarefa", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime?>("DataTerminoEfetivo");
+
+                    b.Property<DateTime>("DataTerminoPlanejado");
+
+                    b.Property<string>("Descricao")
+                        .IsRequired()
+                        .HasAnnotation("MaxLength", 200);
+
+                    b.Property<string>("Email")
+                        .IsRequired()
+                        .HasAnnotation("MaxLength", 100);
+
+                    b.Property<string>("Nome")
+                        .IsRequired()
+                        .HasAnnotation("MaxLength", 100);
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Tarefas");
+                });
+        }
+    }
+}
---
+++ SqlServer/src/ToDoList/Migrations/20161026011213_001.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ToDoList.Migrations
+{
+    public partial class _001 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tarefas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    DataTerminoEfetivo = table.Column<DateTime>(nullable: true),
+                    DataTerminoPlanejado = table.Column<DateTime>(nullable: false),
+                    Descricao = table.Column<string>(maxLength: 200, nullable: false),
+                    Email = table.Column<string>(maxLength: 100, nullable: false),
+                    Nome = table.Column<string>(maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tarefas", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tarefas");
+        }
+    }
+}
